8c1ad1d10a8106c842b72579055b5a3ea2e44aa8
GraceXu12 <149724815+GraceXu12@users.noreply.github.com>
2024-01-27 16:58:43 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+import { StatusBar } from 'expo-status-bar';
+import { Dimensions, StyleSheet, Text, View, Image, ImageBackground, ScrollView, Button, Pressable, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from "react";
+import Clock from './Timer.js';
+
+Clock();
