e9f0ad6ee2ca1ae60a1461ed7fdaa6a06a395c74
GraceXu12 <149724815+GraceXu12@users.noreply.github.com>
2024-01-27 17:07:49 -0500

    Update Timer.js

---
+++ Timer.js
@@ -0,0 +1,98 @@
+import { StatusBar } from 'expo-status-bar';
+import { Dimensions, StyleSheet, Text, View, Image, ImageBackground, ScrollView, Button, Pressable, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from "react";
+
+
+const CathyImg = require("./assets/Cathy.jpg");
+const screen = Dimensions.get("window");
+const formatNumber = number => `0${number}`.slice(-2);
+
+const getRemaining = (time) => {
+  const mins = Math.floor(time/60);
+  const secs = time - mins*60;
+  return { mins: formatNumber(mins), secs: formatNumber(secs)};
+}
+
+export default function Clock() {
+
+  
+
+  const [remainingSecs, setRemainingSecs] = useState(600)
+  const [isActive, setIsActive] = useState(false)
+  const {mins, secs} = getRemaining(remainingSecs)
+
+  const toggle = () => {
+    setIsActive(!isActive);
+  }
+
+  const reset = () => {
+    setRemainingSecs(600);
+    setIsActive(false);
+  }
+
+  useEffect(() =>{
+    let interval = null;
+    if(isActive) {
+      interval = setInterval(() => {
+        setRemainingSecs(remainingSecs => remainingSecs -1);
+      }, 1000)
+    } else if(!isActive && remainingSecs !==0){
+      clearInterval(interval)
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, remainingSecs])
+
+ 
+  return (
+    <View style={styles.container}>
+      <StatusBar style = "light-content"/>
+      <Text style = {styles.timerText}>{`${mins}:${secs}`}</Text>
+      <TouchableOpacity onPress={toggle} style={styles.button}>
+        <Text style = {styles.buttonText}>{isActive ? 'Pause' : "Start"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={reset} style={[styles.button, styles.buttonReset]}>
+        <Text style = {[styles.buttonText, styles.buttonTextReset]}>Reset</Text>
+      </TouchableOpacity>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: '071218',
+      alignItems: "center",
+      justifyContent: 'center',
+  },
+  button: {
+    borderWidth:10,
+    borderColor: "#B9AAFF",
+    width: screen.width/2,
+    height: screen.width/2,
+    borderRadius: screen.width/2,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  buttonText: {
+    fontSize: 45,
+    color: "#B9AAFF"
+  },
+  timerText: {
+    color: "black",
+    fontSize: 90,
+    marginBottom: 20
+  },
+  buttonReset:{
+    marginTop: 20,
+    borderColor: "#FF851B"
+  },
+  buttonTextReset: {
+    color: '#FF851B'
+  }
+
+});
+
+
